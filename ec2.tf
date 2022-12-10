f63fffeaf2a11080da1d9a3168c015ee33a8dc59
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-10 22:21:03 +0530

    third commit

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+    
+}   
+
 resource "aws_instance" "name" {
    ami = "ami1"
    instance_type = "type1"
